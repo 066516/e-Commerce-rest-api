e37d4c3e8fb810965b3dfac596d2851d46394845
066516 <ghmamdjeridinabil@gmail.com>
2024-06-16 23:54:45 +0100

    add controllers

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric',
+            'category_id' => 'required|exists:categories,id',
+        ]);
+
+        $product = Product::create($request->all());
+
+        return response()->json($product, 201);
+    }
+
+    public function show(Product $product)
+    {
+        return response()->json($product);
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric',
+            'category_id' => 'required|exists:categories,id',
+        ]);
+
+        $product->update($request->all());
+
+        return response()->json($product, 200);
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response()->json(null, 204);
+    }
+}
